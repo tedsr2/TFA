77bc54d3b180dfb36b2ae55c3a9375cce799ceeb
Diego Tellier <96288744+tedsr2@users.noreply.github.com>
2022-05-24 21:57:58 +0200

    Header et Dark-mode

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,58 @@
+"use strict"
+
 import { gsap } from "gsap";
 
 console.log(gsap.version);
+
+//Animations
+
+
+
+
+
+//Dark mode
+
+const darkTheme = document.querySelector(".darkmode");
+
+darkTheme.addEventListener("click", function(){
+    if(document.body.dataset.theme === "dark"){
+        light();
+        localStorage.setItem("theme", "light");
+    } else {
+        dark();
+        localStorage.setItem("theme", "dark");
+    } 
+});
+
+const userDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+let theme = localStorage.getItem('theme');
+if((!theme && userDark) || (theme === "dark")){
+    dark();
+} else if(theme === "light"){
+    light();
+}
+
+function dark(){
+    document.body.setAttribute("data-theme", "dark");
+}
+
+function light(){
+    document.body.setAttribute("data-theme", "light");
+}
+
+//Back to top
+
+const backtotopbutton = document.querySelector(".backtotop");
+
+const scrollContainer = () => {
+    return document.documentElement || document.querySelector("body");
+};
+
+const goToTop = () => {
+    document.body.scrollIntoView({
+    behavior: "smooth",
+  });
+};
+
+backtotopbutton.addEventListener("click", goToTop);
