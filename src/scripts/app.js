8411104354a25946a0e6ce6c5ebf4adfff3fd329
Diego Tellier <96288744+tedsr2@users.noreply.github.com>
2022-05-30 23:55:45 +0200

    Footer and Anim

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,13 +1,44 @@
 "use strict"
 
 import { gsap } from "gsap";
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
 console.log(gsap.version);
 
 //Animations
 
+const tl = gsap.timeline({paused: false, defaults: {duration: 1}});
 
+tl.from(".intro__logo", {opacity: 0})
+.to(".intro__animlogo", {yPercent: -2000, duration: 1})
+.to(".intro", {opacity: 0, duration: 0.1})
+.from(".header-anim", {yPercent: -100, duration: 0.5,  onComplete: () => document.querySelector('.content').classList.remove('is-loading')}, "-=0.5")
+.from(".firstlook__titre", {xPercent: -1000}, "-=0.8")
+.from(".firstlook__accroche", {xPercent: -1000}, "-=0.8")
+.from(".firstlook__figure", {xPercent: 1000}, "-=0.8");
+ 
+gsap.registerPlugin(ScrollTrigger);
 
+gsap.from(".preview__textanim--1", {
+    opacity: 0,
+    yPercent: 50,
+    duration: 0.5,
+    scrollTrigger: {
+        trigger: ".preview__square",
+        start: "bottom bottom",
+    },
+ });
+
+gsap.from(".preview__textanim--2", {
+    opacity: 0,
+    yPercent: 50,
+    duration: 0.5,
+    scrollTrigger: {
+        trigger: ".preview__square",
+        start: "bottom bottom",
+    },
+ });
+ 
 
 
 //Dark mode
@@ -56,3 +87,13 @@ const goToTop = () => {
 };
 
 backtotopbutton.addEventListener("click", goToTop);
+
+//Date
+
+const date = document.querySelector('.footer__date');
+
+function setDate() {
+    let date = (new Date).getFullYear();
+    return + date > 2021 ? `2021-${date}` : date
+}
+date.innerText = `${setDate()}`;
