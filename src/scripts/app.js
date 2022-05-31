d7b128254ee07ad4f5f2d2738dd26cea8668eebf
Diego Tellier <96288744+tedsr2@users.noreply.github.com>
2022-06-01 01:48:54 +0200

    Case Study first part

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,16 +3,14 @@
 import { gsap } from "gsap";
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
-console.log(gsap.version);
-
 //Animations
 
 const tl = gsap.timeline({paused: false, defaults: {duration: 1}});
 
 tl.from(".intro__logo", {opacity: 0})
 .to(".intro__animlogo", {yPercent: -2000, duration: 1})
-.to(".intro", {opacity: 0, duration: 0.1})
-.from(".header-anim", {yPercent: -100, duration: 0.5,  onComplete: () => document.querySelector('.content').classList.remove('is-loading')}, "-=0.5")
+.to(".intro", {opacity: 0, duration: 0.1, onComplete: () => document.querySelector('.intro').classList.add('end-loading')})
+.from(".header-anim", {yPercent: -100, duration: 0.5, onComplete: () => document.querySelector('.content').classList.remove('is-loading')}, "-=0.5")
 .from(".firstlook__titre", {xPercent: -1000}, "-=0.8")
 .from(".firstlook__accroche", {xPercent: -1000}, "-=0.8")
 .from(".firstlook__figure", {xPercent: 1000}, "-=0.8");
@@ -38,8 +36,6 @@ gsap.from(".preview__textanim--2", {
         start: "bottom bottom",
     },
  });
- 
-
 
 //Dark mode
 
