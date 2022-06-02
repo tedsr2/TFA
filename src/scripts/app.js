864a1be4c36567a6c5dafd5b9af44e740a5306f8
Diego Tellier <96288744+tedsr2@users.noreply.github.com>
2022-06-02 02:21:05 +0200

    landing et case study v2

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -7,17 +7,18 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 
 const tl = gsap.timeline({paused: false, defaults: {duration: 1}});
 
-tl.from(".intro__logo", {opacity: 0})
+tl.from(".intro__logo", {opacity: 0,})
 .to(".intro__animlogo", {yPercent: -2000, duration: 1})
 .to(".intro", {opacity: 0, duration: 0.1, onComplete: () => document.querySelector('.intro').classList.add('end-loading')})
 .from(".header-anim", {yPercent: -100, duration: 0.5, onComplete: () => document.querySelector('.content').classList.remove('is-loading')}, "-=0.5")
 .from(".firstlook__titre", {xPercent: -1000}, "-=0.8")
 .from(".firstlook__accroche", {xPercent: -1000}, "-=0.8")
+.from(".firstlook__logo", {xPercent: -1000}, "-=0.8")
 .from(".firstlook__figure", {xPercent: 1000}, "-=0.8");
  
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.from(".preview__textanim--1", {
+gsap.from(".preview__square", {
     opacity: 0,
     yPercent: 50,
     duration: 0.5,
@@ -27,15 +28,15 @@ gsap.from(".preview__textanim--1", {
     },
  });
 
-gsap.from(".preview__textanim--2", {
+gsap.from(".firstlook__logo", {
     opacity: 0,
     yPercent: 50,
     duration: 0.5,
     scrollTrigger: {
-        trigger: ".preview__square",
+        trigger: ".firstlook__logo",
         start: "bottom bottom",
     },
- });
+});
 
 //Dark mode
 
